b29b9701a5bd66739d871f260b8c2f0ba3200c3c
powerremit <powerremit@naver.com>
2024-12-10 22:32:45 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+
+import CommentList from "./chapter_05/CommentList";
+
+import Notifications from "./chapter_06/Notification";
+import NotificationList from "./chapter_06/NotificationList";
+import Account from "./chapter_07/Accomodate";
+import Accomodate from "./chapter_07/Accomodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// setInterval(()=>{
+//     root.render(
+//         <React.StrictMode>
+//             {/*<Library />*/}
+//             <Clock />
+//             <CommentList />
+//         </React.StrictMode>
+//     );
+// })
+
+// root.render(
+//         <NotificationList />
+// )
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <Accomodate/>
+        <ConfirmButton/>
+        <LandingPage/>
+     </React.StrictMode>
+)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
