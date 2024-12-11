578113c14c31ae14e570da11803b61f49b3b10ec
powerremit <powerremit@naver.com>
2024-12-11 12:01:20 +0900

    챕터 완료

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
-
 import CommentList from "./chapter_05/CommentList";
-
 import Notifications from "./chapter_06/Notification";
 import NotificationList from "./chapter_06/NotificationList";
-import Account from "./chapter_07/Accomodate";
 import Accomodate from "./chapter_07/Accomodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
+import Signup from "./chapter_11/Signup";
+import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
+import Blocks from "./chapter_15/Blocks";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -37,6 +42,12 @@ root.render(
         <Accomodate/>
         <ConfirmButton/>
         <LandingPage/>
+        <AttendanceBook/>
+        <Signup/>
+        <Calculator/>
+        <ProfileCard/>
+        <DarkOrLight/>
+        <Blocks/>
      </React.StrictMode>
 )
 
